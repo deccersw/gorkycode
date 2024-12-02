a40b4745d69531666044343528be2add352c3d4e
kgugunava <k.gugunava01@gmail.com>
2024-12-02 16:34:24 +0300

    рабочий mvp

--- event.py
+++ event.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField, IntegerField, SelectField, TimeField
+from wtforms import StringField, DateField, SubmitField, IntegerField, SelectField, TimeField, TextAreaField
 from wtforms.validators import DataRequired
 from datetime import datetime
 
@@ -13,11 +13,18 @@ class CreateEvent(FlaskForm):
                                                     ('Любительская игра', 'Любительская игра'), ('Зарядка', 'Зарядка'),
                                                     ('Пробежка', 'Пробежка')],
                                 validators=[DataRequired()])
-    count_of_people = IntegerField('Количество участников', validators=[DataRequired()])
-    time = TimeField('Время', validators=[DataRequired()])
-    date = DateField('Дата', validators=[DataRequired()], default=datetime.today(), format='%Y-%m-%d')
-    place = StringField('Место', validators=[DataRequired()])
-    submit = SubmitField("Создать")
+    count_of_people = IntegerField('Количество участников', validators=[DataRequired()]) # ползунок
+    district =  SelectField('Район города', choices=[("Автозаводский район", "Автозаводский район"), ('Канавинский район', 'Канавинский район'),
+                                                ('Ленинский район', 'Ленинский район'), ('Московский район', 'Московский район'),
+                                                ('Сормовский район', 'Сормовский район'), ('Нижегородский район', 'Нижегородский район'), ('Приокский район', 'Приокский район'),
+                                                ('Советский район', 'Советский район')],
+                            validators=[DataRequired()])
 
-    def str(self):
-        return f'{self.kind_of_sport} {self.type_of_event} {self.count_of_people} {self.date} '
+    place =  SelectField('Спортивные площадки', choices=[("Территория Второго паркового озера", "Территория Второго паркового озера"), ('Территория парка «Дубки»', 'Территория парка «Дубки»'),
+                                                ('Площадка на ул. Лопатина, д. 2, к.1', 'Площадка на ул. Лопатина, д. 2, к.1'), ('Бульвар Юбилейный, 30', 'Бульвар Юбилейный, 30'), ("Территория парка «Светлоярский»", "Территория парка «Светлоярский»"),
+                                                ('ул. Акимова, 47', 'ул. Акимова, 47'), ('л. Сурикова-6', 'ул. Сурикова-6'), ('ул. Бекетова, 69', 'ул. Бекетова, 69'), ('ул. Ванеева, 106', 'ул. Ванеева, 106'),],
+                            validators=[DataRequired()])
+    date = StringField('Дата', validators=[DataRequired()])
+    time = StringField('Время', validators=[DataRequired()])
+    description = TextAreaField("Descr")
+    submit = SubmitField("Опубликовать")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ conn = psycopg2.connect(database='gorkycode',
             host='127.0.0.1',
             port=5432) # подключились к базе данных
 
-id_number = 1 # id мероприятия в базе данных
+# id_number = 1 # id мероприятия в базе данных
 
 # cur = conn.cursor()
 
@@ -47,29 +47,39 @@ app.config['SECRET_KEY'] = 'random string'
 
 @app.route("/")
 def base():
-    return render_template("base.html")
+    return render_template("main_page.html")
 
 
 @app.route("/create", methods=['GET', 'POST'])
 def create():
-    global id_number
+    # global id_number
     form = CreateEvent()
+    # print(form.kind_of_sport.data)
+    # print(form.district.data)
+    # print(form.place.data)
+    # print(form.place.data)
+    # print(form.date.data)
+    # print(form.time.data)
+    # print(form.description.data)
+
     if form.validate_on_submit():
         cur = conn.cursor()
+        # form.count_of_people.data - тоже вернуть     "count_people": []
+        # INSERT INTO events (kind_sport, type_event, count_people, district, place, date, time, description) - ВЕРНУТЬ ОБРАТНО
         cur.execute(
             """
-                INSERT INTO events (id, sport, event_type, count_people, date, time, place)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO events (kind_sport, type_event, count_people, district, place, date, time, description)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """,
-            (id_number, form.kind_of_sport.data, form.type_of_event.data, form.count_of_people.data, form.date.data, form.time.data, form.place.data)
+            (form.kind_of_sport.data, form.type_of_event.data, form.count_of_people.data, form.district.data, form.place.data, form.date.data, form.time.data, form.description.data)
         )
         conn.commit()
         # cur.close()
-        id_number += 1
+        # id_number += 1
 
         return redirect("/")
 
-    return render_template('create.html', form=form)
+    return render_template('index.html', form=form)
 
 @app.route("/created_events")
 def created_events():
@@ -84,14 +94,14 @@ def created_events():
     events_len = len(events)
     # print(len(events))
     
-    dict_of_params = {"kindSport": [], "typeEvent": [], "count_people": [], "date": [], "time": [], "place": []}
+    dict_of_params = {"kindSport": [], "typeEvent": [], "count_people": [], "district": [], "place": [], "date": [], "time": [], "description": []}
     for event in events:
         index = 1
         for param in dict_of_params.keys():
             dict_of_params[param].append(event[index])
             index += 1
     
-    return render_template("created_events.html", dict=dict_of_params, len=events_len)
+    return render_template("view.html", dict=dict_of_params, len=events_len)
 
 
 if __name__ == "__main__":
