f7aa69f609ce90ec0731ecff5e58540c156402fd
nydeg <balabay.08@mail.ru>
2024-12-02 18:10:43 +0300

    версия, которая работает с sqlite3 (у всех)

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect
 from event import CreateEvent
-import psycopg2
+import sqlite3
 
-conn = psycopg2.connect(database='gorkycode',
-            user='postgres',
-            password='postgres',
-            host='127.0.0.1',
-            port=5432) # подключились к базе данных
+conn = sqlite3.connect("basa.db")
 
-# id_number = 1 # id мероприятия в базе данных
+cur = conn.cursor()
 
-# cur = conn.cursor()
+try:
+    cur.execute("""SELECT * from events""").fetchall()
+except Exception as ex:
+    cur.execute(
+        """
+            CREATE TABLE events
+            (
+                id SERIAL PRIMARY KEY,
+                kind_sport CHARACTER VARYING(2000),
+                type_event CHARACTER VARYING(2000),
+                count_people INTEGER,
+                district CHARACTER VARYING(2000),
+                place CHARACTER VARYING(2000),
+                date CHARACTER VARYING(2000),
+                time CHARACTER VARYING(2000),
+                description CHARACTER VARYING(5000)
+            )
+        """
+    )
+    print(ex)
 
-# cur.execute("select * from information_schema.tables where table_name=%s", ('events',))
-# if bool(cur.rowcount) == False:
-#         id_number = 1
-#         cur.execute(
-#             """
-#                 CREATE TABLE events
-#                 (
-#                     id INTEGER,
-#                     sport CHARACTER VARYING(2000),
-#                     event_type CHARACTER VARYING(2000),
-#                     count_people INTEGER,
-#                     date CHARACTER VARYING(2000),
-#                     time CHARACTER VARYING(2000),
-#                     place CHARACTER VARYING(2000)
-#                 )
-#             """
-#         )
 
-# else:
-#     cur.execute(
-#         """
-#             SELECT * FROM events
-#         """
-#     )
-#     events = cur.fetchall()[-1]
-#     id_number = events[0] + 1
-
-# conn.commit()
+conn.commit()
+conn.close()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'random string'
@@ -52,30 +42,24 @@ def base():
 
 @app.route("/create", methods=['GET', 'POST'])
 def create():
-    # global id_number
+    global id_number
     form = CreateEvent()
-    # print(form.kind_of_sport.data)
-    # print(form.district.data)
-    # print(form.place.data)
-    # print(form.place.data)
-    # print(form.date.data)
-    # print(form.time.data)
-    # print(form.description.data)
 
     if form.validate_on_submit():
+        conn = sqlite3.connect("basa.db")
         cur = conn.cursor()
         # form.count_of_people.data - тоже вернуть     "count_people": []
         # INSERT INTO events (kind_sport, type_event, count_people, district, place, date, time, description) - ВЕРНУТЬ ОБРАТНО
         cur.execute(
             """
                 INSERT INTO events (kind_sport, type_event, count_people, district, place, date, time, description)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
             """,
-            (form.kind_of_sport.data, form.type_of_event.data, form.count_of_people.data, form.district.data, form.place.data, form.date.data, form.time.data, form.description.data)
+            (form.kind_of_sport.data, form.type_of_event.data, form.count_of_people.data, form.district.data,
+             form.place.data, form.date.data, form.time.data, form.description.data)
         )
         conn.commit()
-        # cur.close()
-        # id_number += 1
+        conn.close()
 
         return redirect("/")
 
@@ -83,6 +67,7 @@ def create():
 
 @app.route("/created_events")
 def created_events():
+    conn = sqlite3.connect("basa.db")
     cur = conn.cursor()
     cur.execute(
         """
@@ -94,7 +79,8 @@ def created_events():
     events_len = len(events)
     # print(len(events))
     
-    dict_of_params = {"kindSport": [], "typeEvent": [], "count_people": [], "district": [], "place": [], "date": [], "time": [], "description": []}
+    dict_of_params = {"kindSport": [], "typeEvent": [], "count_people": [], "district": [], "place": [], "date": [],
+                      "time": [], "description": []}
     for event in events:
         index = 1
         for param in dict_of_params.keys():
